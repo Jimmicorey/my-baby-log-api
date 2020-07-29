85ef6b10651ca3236cb8d06189ed84918b2e9844
James C. Coffelt <james.c.coffelt@gmail.com>
2020-07-29 15:06:08 -0500

    initial sever test runs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
-const app = require('./app')
-const { PORT } = require('./config')
+/* eslint-disable strict */
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
