d2bc5851d809aa4d885861bc8d3adacd0e83dc13
James C. Coffelt <james.c.coffelt@gmail.com>
2020-08-04 18:30:17 -0500

    updates

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/my_baby_log_db',
-  CLIENT_ORIGIN: 
+  DATABASE_URL: process.env.REACT_APP_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/my_baby_log_db',
+  //CLIENT_ORIGIN: 
 };
