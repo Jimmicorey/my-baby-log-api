e9363f1815bc6f81236765891fd3d52172ff21ea
James C. Coffelt <james.c.coffelt@gmail.com>
2020-08-04 18:21:05 -0500

    updates

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://qnirjztwpfrlrz:52b1f92461c99774b497c9b30e8638e2b064ed18d08319683643463338fac79a@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d8cojkgac7k4d7',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/my_baby_log_db',
   CLIENT_ORIGIN: 
 };
