523de2a86e96e445d971fc8776f79b9cee11de8a
James C. Coffelt <james.c.coffelt@gmail.com>
2020-08-05 09:55:43 -0500

    updated SEED data

--- seeds/seed.my_baby_log_events.sql
+++ seeds/seed.my_baby_log_events.sql
@@ -7,13 +7,13 @@ TRUNCATE
 
 INSERT INTO my_baby_log_events (event_category)
 VALUES
-  ('Feed 1'),
-  ('Bath 1'),
-  ('Diaper 1'),
-  ('Feed 2'),
-  ('Bath 2'),
-  ('Diaper 2'),
-  ('Feed 3'),
-  ('Bath 3'),
-  ('Diaper 3');
+  ('Feed'),
+  ('Bath'),
+  ('Diaper'),
+  ('Feed'),
+  ('Bath'),
+  ('Diaper'),
+  ('Feed'),
+  ('Bath'),
+  ('Diaper');
 COMMIT;
